67455c33bbd19da041b56ee45f496f4ba54cbedf
Anshuman <anshumanmsihhra1@gmail.com>
2023-11-20 14:49:33 +0530

    tutorial55

---
+++ tutorial55/script.js
@@ -0,0 +1,42 @@
+console.log("hey this tutorial 55")
+
+//javascript is a case sensitive language
+let a=5;
+a=a+1;
+
+{
+    let a=66;
+    console.log(a);
+}
+
+console.log(a)
+ 
+var b=6;
+var c="harry";
+// var 55a=4;  no applicable 
+
+console.log(a+b)
+console.log(typeof a, typeof b, typeof c)
+
+// var is globally scoped while let is blocked scoped
+
+// const a1=6;
+// a1=a1+1; it's not allowed 
+
+let x=" harry bhai";
+let y =22;
+let z=3.35;
+const p = true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    name : "harry",
+    "job code" :560,
+    "is_handsome" :true
+}
+o.salary="1000crores"
+console.log(o)

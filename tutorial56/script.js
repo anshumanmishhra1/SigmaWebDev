669919949dec8ae92e08514dd55d1872ef8d4bd9
Anshuman <anshumanmsihhra1@gmail.com>
2023-11-22 18:19:21 +0530

    tutorial56

---
+++ tutorial56/script.js
@@ -0,0 +1,15 @@
+console.log("hye I am conditional tutorial")
+
+let age=7;
+let grace=2;
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age**grace);
+if(age+grace>18){
+    console.log("you can drive");
+}
+else{
+    console.log("You can't drive");
+}

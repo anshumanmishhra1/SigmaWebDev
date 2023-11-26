4e26606905f6747993c5a17b75bf69613654cc9f
Anshuman <anshumanmsihhra1@gmail.com>
2023-11-26 18:14:14 +0530

    tutorial58

---
+++ tutorial58/script.js
@@ -0,0 +1,33 @@
+// console.log("Hey this function in JavaScript");
+
+/*
+function nice(name){
+    console.log("hey " + name + " you are nice");
+    console.log("hey " + name + "you are nice");
+    console.log("Hey " + name + "Your tshirt is good");
+    console.log("hey "+ name + "Your course is good");
+}
+
+nice("rohan");
+nice("shivam");
+
+*/
+
+
+/* function to add */ 
+function sum(a,b){
+    // console.log(a+b);
+    console.log("the sum of two is :",a+b);
+}
+
+result=sum(2,3);
+// console.log("the sum of these number is : ",result);
+
+
+const func1 =(x)=>{
+    console.log("I am an arrow function",x);
+}
+
+func1(34);
+func1(64);
+func1(74);

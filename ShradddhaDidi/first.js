53b6b70f685e1cfe34d85df33ce28d2a6c6ffd84
Anshuman <anshumanmsihhra1@gmail.com>
2023-12-04 23:10:24 +0530

    day1

---
+++ ShradddhaDidi/first.js
@@ -0,0 +1,30 @@
+// console.log("Welcome to Apna college");
+// console.log("Anshuman");
+// console.log("I love JavaScritp");
+
+
+fullname="tony stark";
+age=24;
+price=34;
+x=null;
+y=undefined;
+isFollow=true;
+fullname=25;
+console.log(fullname);
+
+//variable names are case sensitive
+
+fullNam="shraddha";
+fullnam="anshuman";
+console.log(fullnam);
+console.log(fullNam);
+
+//let,const &var
+
+// var : variable can be re-declared & updated, A global scope variable
+
+// let : variable canot be re-declared but can be updated, a lcok scope bariable.A
+
+// const : varibale cannot be re-declared or updated, A block  scoopre variable
+
+

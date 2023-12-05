5499afe52f98f6ffd740a34585481a037ebb57df
Anshuman <anshumanmsihhra1@gmail.com>
2023-12-05 13:38:06 +0530

    tutorial67

---
+++ tutorial67/script.js
@@ -0,0 +1,8 @@
+console.log("This is Dom Lecture 2");
+
+// to find childNodes we use this : but it onnly works in console
+// document.body.childNodes[0];
+
+document.body.firstElementChild // first element child
+document.body.firstElementChild.childNodes // all nodes comments, nodes, text, element
+document.body.firstElementChild.children //all element nodes

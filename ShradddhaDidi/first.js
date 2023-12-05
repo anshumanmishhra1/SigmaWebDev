f0724b05f3c6d7f12ff2b17a81a9fdb56d24c0cf
Anshuman <anshumanmsihhra1@gmail.com>
2023-12-05 13:58:17 +0530

    day1a

--- ShradddhaDidi/first.js
+++ ShradddhaDidi/first.js
@@ -1,30 +1,70 @@
-// console.log("Welcome to Apna college");
-// console.log("Anshuman");
-// console.log("I love JavaScritp");
+// // console.log("Welcome to Apna college");
+// // console.log("Anshuman");
+// // console.log("I love JavaScritp");
 
 
-fullname="tony stark";
-age=24;
-price=34;
-x=null;
-y=undefined;
-isFollow=true;
-fullname=25;
-console.log(fullname);
+// fullname="tony stark";
+// age=24;
+// price=34;
+// x=null;
+// y=undefined;
+// isFollow=true;
+// fullname=25;
+// console.log(fullname);
 
-//variable names are case sensitive
+// //variable names are case sensitive
 
-fullNam="shraddha";
-fullnam="anshuman";
-console.log(fullnam);
-console.log(fullNam);
+// fullNam="shraddha";
+// fullnam="anshuman";
+// console.log(fullnam);
+// console.log(fullNam);
 
-//let,const &var
+// //let,const &var
 
-// var : variable can be re-declared & updated, A global scope variable
+// // var : variable can be re-declared & updated, A global scope variable
 
-// let : variable canot be re-declared but can be updated, a lcok scope bariable.A
+// // let : variable canot be re-declared but can be updated, a lcok scope bariable.A
 
-// const : varibale cannot be re-declared or updated, A block  scoopre variable
+// // const : varibale cannot be re-declared or updated, A block  scoopre variable
 
 
+
+// const obj_name={
+//     age:24,
+//     fullName:"Rahul Kumar",
+//     cgpa:8.2,
+//     isPass:true
+// };
+
+// obj_name["age"]=obj_name["age"]+1;
+
+// console.log(obj_name["age"]);
+// console.log(obj_name.age)
+
+
+// 📌practice 1: create a const object called product to 
+// store information shown in the picture.
+
+// const product={
+//     title:"Ball Pen",
+//     rating:4,
+//     offer:5,
+//     price:270,
+// };
+
+// console.log(product);
+
+
+
+// 📌Practice 2 : Create a const object called "profile" to
+// store information shown in the picture
+
+const profile={
+    name:"Anshuman Mishra",
+    isFollow:true,
+    posts: 195,
+    followers: 596,
+    following:4,
+};
+
+console.log(profile)

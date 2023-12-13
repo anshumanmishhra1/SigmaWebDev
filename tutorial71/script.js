85b548d8f2fbefa729eeed3c67f0a934d2c3a6ea
Anshuman <anshumanmsihhra1@gmail.com>
2023-12-13 14:16:32 +0530

    tutorial 71

---
+++ tutorial71/script.js
@@ -0,0 +1,20 @@
+// console.log("Insertion and deletion in DOM");
+
+
+/*console 1
+// document.querySelector(".box")
+<div class=​"box">​Hey i am a box ​</div>​
+document.querySelector(".box").inne
+undefined
+document.querySelector(".box").innerHTML
+'Hey i am a box\n            \n        '
+document.querySelector(".container").innerHTML
+'\n        <div class="box">Hey i am a box\n            \n        </div>\n    '
+document.querySelector(".container").innerText
+'Hey i am a box'
+document.querySelector(".box").innerText
+'Hey i am a box'
+
+
+*/
+
